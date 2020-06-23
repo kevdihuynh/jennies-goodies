d8e53ea1d118273f1f0eec0792e8d4255f532d89
Johnson Huynh <huynhjjk@gmail.com>
2020-06-23 06:47:42 -0700

    fixing up footer

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalConstants } from './../../utils/global-constants';
-
+import * as _ from 'lodash';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -8,6 +8,38 @@ import { GlobalConstants } from './../../utils/global-constants';
 })
 export class FooterComponent implements OnInit {
   public globalConstants = GlobalConstants;
+  public socialMedia = [
+    {
+      title: 'Facebook',
+      iconClass: 'fab fa-facebook fa-2x text-dark',
+      url: `https://www.facebook.com/Jennies-Goodies-112088677209362/`
+    },
+    {
+      title: 'LinkedIn',
+      iconClass: 'fab fa-instagram fa-2x text-dark',
+      url: `https://www.instagram.com/jenniesgoodies/`
+    },
+    {
+      title: 'Email',
+      iconClass: 'fas fa-envelope fa-2x text-dark',
+      url: `mailto:jenniesgoodies1994@gmail.com?Subject=Hey Jennie`
+    }
+  ];
+
+  public links = [
+      {
+        title: 'About',
+        url: '#/about'
+      },
+      {
+        title: 'Privacy',
+        url: '#/privacy'
+      },
+      {
+        title: 'Terms',
+        url: '#/terms'
+      }
+  ];
 
   constructor() { }
 
