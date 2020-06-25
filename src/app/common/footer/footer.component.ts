aeb3ee0473a01c7cdc066ceb9c1b4a49fd0b2be6
Johnson Huynh <huynhjjk@gmail.com>
2020-06-25 08:19:39 -0700

    adding click to open phone

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -12,17 +12,30 @@ export class FooterComponent implements OnInit {
     {
       title: 'Facebook',
       iconClass: 'fab fa-facebook fa-2x text-dark',
-      url: `${this.globalConstants.company.facebook}`
+      url: `${this.globalConstants.company.facebook}`,
+      displayTitle: false,
+      openTab: true
     },
     {
       title: 'Instagram',
       iconClass: 'fab fa-instagram fa-2x text-dark',
-      url: `${this.globalConstants.company.instagram}`
+      url: `${this.globalConstants.company.instagram}`,
+      displayTitle: false,
+      openTab: true
     },
     {
       title: 'Email',
       iconClass: 'fas fa-envelope fa-2x text-dark',
-      url: `mailto:${this.globalConstants.company.email}?Subject=Hey Jennie`
+      url: `mailto:${this.globalConstants.company.email}?Subject=Hey Jennie`,
+      displayTitle: false,
+      openTab: true
+    },
+    {
+      title: this.globalConstants.company.phoneNumber,
+      iconClass: 'fas fa-phone fa-2x text-dark',
+      url: `tel:${this.globalConstants.company.phoneNumber}`,
+      displayTitle: true,
+      openTab: false
     }
   ];
 
