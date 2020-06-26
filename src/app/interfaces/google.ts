5dc648dbabbd0e15f34818901bd2435d104c7e66
Johnson Huynh <huynhjjk@gmail.com>
2020-06-26 10:58:25 -0700

    adding new files

---
+++ src/app/interfaces/google.ts
@@ -0,0 +1,12 @@
+export interface FormattedAddressComponent {
+    administrative_area_level_1?: string;
+    administrative_area_level_2?: string;
+    country?: string;
+    locality?: string;
+    neighborhood?: string;
+    political?: string;
+    postal_code?: string;
+    postal_code_suffix?: string;
+    route?: string;
+    street_number?: string;
+}
