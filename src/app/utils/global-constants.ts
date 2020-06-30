d01a89d2aa83aca3ffdad6cb00e0cb1b45c20a5a
Kevin Huynh <kevdihuynh@gmail.com>
2020-06-29 21:37:16 -0700

    [PROD] adding in copyright names for resume

--- src/app/utils/global-constants.ts
+++ src/app/utils/global-constants.ts
@@ -1,7 +1,7 @@
 export class GlobalConstants {
     public static apiURL: string = "https://wwww.itsolutionstuff.com/";
     public static company = {
-        name: "Jennie's Goodies",
+        name: "Johnson Huynh and Kevin Huynh",
         slogan: "Fresh homemade pastries & sweets served every day!",
         email: 'jenniesgoodies@yahoo.com',
         logo: '../assets/images/jennies_goodies_logo.jpg',
