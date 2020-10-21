6492d03f078febdc2f5908c4408c5cd0a2b8d5ba
Johnson Huynh <huynhjjk@gmail.com>
2020-10-21 10:16:45 -0700

    adding X for

--- src/app/common/cart-button/cart-modal/paypal/paypal.component.ts
+++ src/app/common/cart-button/cart-modal/paypal/paypal.component.ts
@@ -65,7 +65,7 @@ export class PaypalComponent implements OnInit {
         const getItems = (): ITransactionItem[] => {
             const items = _.map(this.orderForm.orders, (order: Order): any => {
                 return {
-                    name: `${order.quantity} x ${order.name}  (${_.join(order.selectedFlavors, ', ')})`,
+                    name: `${order.quantity} x ${order.name} (${_.join(order.selectedFlavors, ', ')}) - ${order.batchSize} for $${order.price}`,
                     quantity: order.quantity,
                     category: 'PHYSICAL_GOODS',
                     unit_amount: {
