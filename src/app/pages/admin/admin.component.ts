e61afd3f0e265c81bc87dec7d73a63807aa07737
Johnson Huynh <huynhjjk@gmail.com>
2020-12-20 20:07:45 -0800

    admin

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import * as _ from 'lodash';
 import { GlobalConstants } from '../../utils/global-constants';
+import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-admin',
@@ -11,10 +12,12 @@ import { GlobalConstants } from '../../utils/global-constants';
 })
 export class AdminComponent implements OnInit {
   public _ = _;
+  public moment = moment;
   public input: string;
   public globalConstants: any = GlobalConstants;
   public transactions: Array<any>;
   public totalProfit: number = 0;
+  public selectedMode: string = 'all';
 
   constructor(firestore: AngularFirestore) {
     firestore.collection('transactions').valueChanges().subscribe((transactions) => {
@@ -26,5 +29,31 @@ export class AdminComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  isTodaySales(transaction: any): boolean {
+    const dateSelected = _.get(transaction, ['orderForm', 'selectedDateTime', 'start', 'dateTime']);
+    return moment(moment(dateSelected).format('YYYY-MM-DD')).isSame(moment().format('YYYY-MM-DD'))
+  }
+
+  isCompletedSales(transaction: any): boolean {
+    const dateSelected = _.get(transaction, ['orderForm', 'selectedDateTime', 'start', 'dateTime']);
+    return moment(moment(dateSelected).format('YYYY-MM-DD')).isBefore(moment().format('YYYY-MM-DD'))
+  }
+
+  getFilteredTransactions(): Array<any> {
+    switch (_.toLower(this.selectedMode)) {
+      case 'today':
+        return _.filter(this.transactions, (transaction: any) => this.isTodaySales(transaction));
+      case 'active':
+      return _.filter(this.transactions, (transaction: any) => {
+        const dateSelected = _.get(transaction, ['orderForm', 'selectedDateTime', 'start', 'dateTime']);
+        return moment(moment(dateSelected).format('YYYY-MM-DD')).isAfter(moment().format('YYYY-MM-DD'))
+      });
+    case 'completed':
+      return _.filter(this.transactions, (transaction: any) => this.isCompletedSales(transaction));
+    default:
+      return this.transactions;
+    }
   }
 }
